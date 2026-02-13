fcb85bb93e3c5e4dc5ab8fa6892e475b3a20f955
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-13 16:57:10 +0100

    Inizializzo la base URL dell’API e lo state per salvare la lista degli attori

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  // BASE URL
+  const baseUrl = "https://lanciweb.github.io/demo/api/actors/";
+  const [characters, setCharacters] = useState([]);
 
   return (
     <>
@@ -14,4 +17,4 @@ function App() {
   )
 }
 
-export default App
+
